8c5fc9014a8b203e9913597d93ff74f4ae2d1899
nafizzzzzzzzzz <nhremon8181@gmail.com>
2020-05-14 03:33:24 +0600

    Added multiple vehicle resource

--- src/VehicleDatabaseApp.java
+++ src/VehicleDatabaseApp.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class VehicleDatabaseApp {
 
     public static void main(String[] args) {
+        System.out.println("How many vehicles do you want to Add:");
+        Scanner inp =  new Scanner(System.in);
+        int numOfVehicle = inp.nextInt();
+        Vehicle[] vehicles= new Vehicle[numOfVehicle];
+        for (int i=0; i<numOfVehicle; i++)
+        {
 
-        Vehicle v1 =  new Vehicle();
+            vehicles[i] =  new Vehicle();
+        }
+
+        for (int i=0; i<numOfVehicle; i++)
+        {
+
+            System.out.println(vehicles[i].printAll());
+        }
+
+
         //add any type of vehicle
+
     }
 
 
