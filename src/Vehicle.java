ad4cd394efc7852f03c71cadd6374a4bdc76cae8
nafizzzzzzzzzz <nhremon8181@gmail.com>
2020-05-14 02:55:29 +0600

    Added vehicle Type

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -7,6 +7,9 @@ public class Vehicle {
     private float tyreSize;
     private int ID = 101;
     private String vehicleId;
+    public int visitor = 0;
+
+    private String vehicleTypes;
     /*  constructor: vehicle has attributes of - -
                                                 1. model number
                                                 2. engine type
@@ -29,10 +32,22 @@ public class Vehicle {
 
         setVehicleId();
 
+        SetType();
+
         System.out.println("Model Number:"+ modelNumber + " Engine type:"+ engineType + " Engine Power:"+ enginePower+ " Tyre Size:"+ tyreSize);
         System.out.println("ID "+ vehicleId);
+
+        System.out.println(vehicleTypes);
+        System.out.println("Visitors are: "+ visitor);
+
     }
 
+    //setting each vehicle with distinct id
+    private void setVehicleId(){
+        ID++;
+        this.vehicleId= modelNumber+ "" + ID;
+
+    }
 
 
     /*
@@ -46,12 +61,67 @@ public class Vehicle {
                  b. engine type diesel only
 
      */
+    private void SetType(){
+        //set the three types of vehicles for counting the visitors
+        System.out.println("Enter the Type of vehicle (Q for quit): " +
+                "\n\t1. Normal- No extra property" +
+                "\n\t2. Sports- " +
+                "\n\t\ta. Turbo" +
+                "\n\t\ta. Engine type oil only" +
+                "\n\t3.Heavy-" +
+                "\n\t\ta.Weight" +
+                "\n\t\tb. Engine type diesel only");
+        String type1 = "Normal";
+        String type2 = "Sports";
+        String type2a = "Turbo";
+        String type2b = "Engine type oil only";
+        String type3 = "Heavy";
+        String type3a = "Weight";
+        String type3b = "Engine type diesel only";
 
-    //setting each vehicle with distinct id
-    private void setVehicleId(){
-        ID++;
-        this.vehicleId= modelNumber+ "" + ID;
+        Scanner inp = new Scanner(System.in);
+        String vehicle_type = inp.nextLine();
+        if (!vehicle_type.equals("Q")){
+            //1.Normal type
+            if (vehicle_type.toLowerCase().equals(type1.toLowerCase())||vehicle_type.equals("1")){
+                vehicleTypes = "Vehicle Type: " + type1;
+            }
+            else if (vehicle_type.toLowerCase().equals(type2.toLowerCase())||vehicle_type.equals("2")){
+                //2.Sports
+                System.out.println("ESEchi");
+                vehicleTypes = "Vehicle Type: " + "(" +  type2 ;
+                vehicle_type = inp.nextLine();
+                if (vehicle_type.toLowerCase().equals(type2a.toLowerCase())||vehicle_type.equals("a")){
+                   //2a. Turbo
+                    vehicleTypes = vehicleTypes + type2a + ")";
+                }
+                else if (vehicle_type.toLowerCase().equals(type2b.toLowerCase())||vehicle_type.equals("b")){
 
+                    //2b. Engine type oil
+                    vehicleTypes = vehicleTypes+ type2b +")";
+                }
+            }
+            else if (vehicle_type.toLowerCase().equals(type3.toLowerCase())||vehicle_type.equals("3")){
+                //2.Sports
+                vehicleTypes = "Vehicle Type: " + "(" +type3;
+                vehicle_type = inp.nextLine();
+                if (vehicle_type.toLowerCase().equals(type3a.toLowerCase())||vehicle_type.equals("a")){
+                    //2a. Turbo
+                    vehicleTypes = vehicleTypes + type3a + ")";
+                }
+                else if (vehicle_type.toLowerCase().equals(type3b.toLowerCase())||vehicle_type.equals("b")){
+
+                    //2b. Engine type oil
+                    vehicleTypes = vehicleTypes+ type3b +")";
+                }
+            }
+
+            visitor = visitor + 10;
+
+        }
+        else {
+            System.out.println("Invalid Character");
+        }
     }
 
 
